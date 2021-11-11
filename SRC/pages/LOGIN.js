107917ad8c5961bb5797895695b879bfe5b4b894
Ampong K <ampongk90@gmail.com>
2021-11-11 22:56:04 +0000

    progress

---
+++ SRC/pages/LOGIN.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View , Image, SafeAreaView, TouchableOpacity, Alert, Button} from 'react-native';
+
+export default class Login extends Component<{}>{
+    render(){
+        return(
+            <View>
+            <Text> Login </Text> 
+            </View>
+
+        )
+
+
+    }
+
+
+
+}
+
+
