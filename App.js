6d02b435dfdec0affad4bc7443047d9b4a4ee9d3
Ampong K <83551356+KwabenaAmpong@users.noreply.github.com>
2021-11-17 23:51:45 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ export default function App() {
       <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
-            initialRouteName="Home"
+            initialRouteName="Login"
             screenOptions={{ headerShown: false }}
           >
             <Stack.Screen name="Login" component={Login} />
