6f1888d35561f66b0762b8f4629ee9d232b04e87
Ampong K <ampongk90@gmail.com>
2021-11-17 23:39:22 +0000

    commit

--- app/screen/Login.js
+++ app/screen/Login.js
@@ -31,7 +31,7 @@ const Login = ({ navigation }) => {
     >
       <Image
         source={{
-          uri: "https://media.istockphoto.com/photos/variety-of-fresh-organic-vegetables-and-fruits-in-the-garden-picture-id1280856062?b=1&k=20&m=1280856062&s=170667a&w=0&h=wQu-c2ZjzeCBkAGEj69xpF611lx1i_xim48vOCj_Dw0=",
+          uri: "https://images.unsplash.com/photo-1459478309853-2c33a60058e7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGlnaCUyMHJlc29sdXRpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         }}
         style={{
           width: 180,
