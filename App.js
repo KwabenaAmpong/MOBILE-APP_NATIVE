429defbbf1e6a37a3dc6efdb8648a9fd029b6cf4
Ampong K <83551356+KwabenaAmpong@users.noreply.github.com>
2021-11-17 22:41:29 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View , Image, SafeAreaView, TouchableOpacity, Alert, Button} from 'react-native';
-import  LOGIN from  './SRC/pages/LOGIN';
+import React from "react";
+
+//importing React redux packages
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+//import for the setting up of screen navigation
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+//imports to handle fonts from expo fonts
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+// screen imports from ./screen
+import Login from "./app/screen/Login";
+import Home from "./app/screen/Home";
+import ProductDetails from "./app/screen/ProductDetails";
+
+import allReducers from "./app/redux/store";
+import Cart from "./app/screen/Cart";
 
 export default function App() {
- 
- const forpress = () => console.log('Pic Pressed');
-  return (
-    <SafeAreaView style={styles.container}>
+  const Stack = createStackNavigator();
 
-     <Text>LOGIN TO MY APP</Text>
-      <TouchableOpacity onPress={forpress}> 
-        
-      <Image source={{
-       width:200,
-       height:210, 
-        
-       uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSmCAhdKIfdkKTivymRAZpOfN9rsNILUWoJg&usqp=CAU"}}/> 
-        
-        </TouchableOpacity>
-        <Button color="black" title="CLICK TO LOGIN" 
-        onPress
-       />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+  let [fontsLoaded] = useFonts({
+    "Zen-Bold": require("./app/assets/fonts/Zen-Bold.ttf"),
+    "Zen-Regular": require("./app/assets/fonts/Zen-Regular.ttf"),
+    "Zen-Light": require("./app/assets/fonts/Zen-Light.ttf"),
+  });
+
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    allReducers,
+    composeEnhancers(applyMiddleware(thunk))
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-  
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="ProductDetails" component={ProductDetails} />
+            <Stack.Screen name="Cart" component={Cart} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
+}
