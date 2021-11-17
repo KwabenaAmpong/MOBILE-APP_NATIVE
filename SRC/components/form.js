27fede1ad919fef903825f8c3b6af557439a11fa
Ampong K <ampongk90@gmail.com>
2021-11-17 22:32:32 +0000

    commit

--- SRC/components/form.js
+++
@@ -1,6 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View , Image, SafeAreaView, TouchableOpacity, Alert, Button} from 'react-native';
-
-
-
